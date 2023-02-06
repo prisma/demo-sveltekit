58d0475ebc576b7999fb529dd28beef2a016a546
sonylomo <sonylomo1@gmail.com>
2023-02-06 21:08:39 +0300

    feat: intial demo template

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import Post from '$lib/components/Post.svelte'
+  // import type { PageData } from './$types'
+  import Data from "../lib/data.json"
+  
+  // data returned from +page.server.js
+
+
+</script>
+
+<div>
+  <h1>My Blog</h1>
+  <main>
+    <div>
+      {#each Data as post (post.id)}
+        <Post {post} />
+      {/each}
+    </div>
+  </main>
+</div>
---
+++ src/routes/signup/+page.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+
+</script>
+
+<div class="page">
+  <form method="post">
+    <h1>Signup user</h1>
+    <input
+      placeholder="Name"
+      type="text"
+      name="name"
+    />
+    <input
+      placeholder="Email address"
+      type="text"
+      name="userEmail"
+    />
+    <button type="submit">Signup</button>
+
+    <a class="back" href="/"> or Cancel </a>
+  </form>
+</div>
+
+<style scoped>
+  .page {
+    background: white;
+    padding: 3rem;
+    display: flex;
+    justify-content: center;
+  }
+
+  input[type="text"] {
+    width: 100%;
+    padding: 0.5rem;
+    margin: 0.5rem 0;
+    border-radius: 0.25rem;
+    border: 0.125rem solid rgba(0, 0, 0, 0.2);
+  }
+
+  button[type="submit"] {
+    background: #ececec;
+    border: 0;
+    padding: 1rem 2rem;
+  }
+
+  .back {
+    margin-left: 1rem;
+  }
+  .error {
+    color: red;
+  }
+</style>
